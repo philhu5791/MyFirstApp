75dd5909869dbb805b7eb7814723e3951179dfeb
philhu5791 <philhu5791@gmail.com>
2013-07-19 22:04:21 -0500

    test for single file changed

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        //test
+        int i;
         return true;
     }
     
